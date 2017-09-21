3b4410794f562a6f1718af01bb2d58a1712123a5
phatcao <32162561+phatcao@users.noreply.github.com>
2017-09-21 17:18:56 +0700

    Add files via upload

---
+++ riss/js/myjs.js
@@ -0,0 +1,8 @@
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'fade',
+    autoplay: 2500,
+    autoplayDisableOnInteraction: false
+  })        
